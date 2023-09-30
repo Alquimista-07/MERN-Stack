506f751473828cda429fa472022752ea5aa531d2
Alquimista-07 <jdca.07@gmail.com>
2023-09-30 07:34:05 -0500

    Stack MERN #3, Conexion a la Base de datos Mongodb

--- backend/src/database.js
+++ backend/src/database.js
@@ -1,1 +1,30 @@
-// Configuración de la conexión a la base de datos.
+// Este archivo continen la configuración de la conexión con la base de datos
+// mongoose
+
+const mongoose = require('mongoose');
+
+// Creamos una función que sea la encargada de establecer la conexión
+const dbConnection = async() => {
+
+    try {
+        
+        // la función connect recibe la cadena de conexión proveniente de una variable de entorno.
+        await mongoose.connect( process.env.DB_CNN , {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+
+        console.log( 'DB Online!!!...' );
+
+    }
+    catch (error) {
+        console.log(error);
+        throw new Error('Error a la hora de inicializar la DB revisar logs');
+    }
+
+
+}
+
+module.exports = {
+    dbConnection
+}
--- backend/src/index.js
+++ backend/src/index.js
@@ -2,11 +2,20 @@
 
 const app = require('./app');
 
+// Para congigurar las variables de entorno configuramos usando el paquete dotenv.
+require('dotenv').config();
+
+// Importamos el archivo de configuración de la base de datos
+const { dbConnection } = require('./database');
+
 // Arrancamos el servidor usando una función asíncrona
 async function main() {
-    await app.listen(4000);
-    console.log('Servidor ejecutandose en el puerto 4000');
+    await app.listen(process.env.PORT);
+    console.log('Servidor ejecutandose en el puerto: ' + process.env.PORT);
 }
 
 // Llamamos la función.
 main();
+
+// Base de datos
+dbConnection();
