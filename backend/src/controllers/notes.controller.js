872af4f44e57399ebc5b3ec5a9c26a8beddbcfbe
Alquimista-07 <jdca.07@gmail.com>
2023-09-30 11:16:33 -0500

    Stack MERN #7, Mongodb Queries con Mongoose

--- backend/src/controllers/notes.controller.js
+++ backend/src/controllers/notes.controller.js
@@ -1,23 +1,74 @@
+const NoteModel = require('../models/Note');
+
 const notesCtrl = {};
 
-notesCtrl.getNotes = (req, res) => res.json({
-    message: []
-});
+notesCtrl.getNotes = async (req, res) => {
 
-notesCtrl.getNote = (req, res) => res.json({
-    message: "sdfsdf"
-});
+    // Devolvemos todas las notas
+    const notes = await NoteModel.find();
 
-notesCtrl.createNote = (req, res) => res.json({
-    message: 'Nota guardada'
-});
+    res.json({
+        notes
+    });
+};
 
-notesCtrl.updateNote = (req, res) => res.json({
-    message: 'Nota actualizada'
-});
+notesCtrl.getNote = async (req, res) => {
 
-notesCtrl.deleteNote = (req, res) => res.json({
-    message: 'Nota borrada'
-});
+    const id = (req.params.id);
+
+    const note = await NoteModel.findById(id);
+
+    console.log(note);
+
+    res.json({
+        note
+    });
+};
+
+notesCtrl.createNote = async (req, res) => {
+
+    const { title, content, date, author } = req.body;
+
+    const newNote = new NoteModel({
+        title,
+        content,
+        date,
+        author
+    });
+
+    await newNote.save();
+
+    res.json({
+        message: "Nota Guardada"
+    });
+};
+
+notesCtrl.updateNote = async (req, res) => {
+
+    const id = req.params.id;
+    const { title, content, author } = req.body;
+
+    await NoteModel.findOneAndUpdate( { _id: id }, {
+        title,
+        content,
+        author
+    });
+
+    res.json({
+        message: 'Nota actualizada!'
+    });
+};
+
+notesCtrl.deleteNote = async (req, res) => {
+    
+    const id = req.params.id;
+
+    await NoteModel.findByIdAndDelete(id);
+
+    res.json({
+        message: 'Nota borrada!'
+    });
+};
+
 
 module.exports = notesCtrl;
--- backend/src/controllers/users.controllers.js
+++ backend/src/controllers/users.controllers.js
@@ -1,15 +1,40 @@
+const UserModel = require('../models/User');
+
 const userCtrl = {};
 
-userCtrl.getUsers = (req, res) => res.json({
-    message: []
-});
+userCtrl.getUsers = async (req, res) => {
 
-userCtrl.createUser = (req, res) => res.json({
-    message: 'Usuario guardada'
-});
+    const users = await UserModel.find();
 
-userCtrl.deleteUser = (req, res) => res.json({
-    message: 'Usuario borrada'
-});
+    res.json({
+        users
+    });
+};
+
+userCtrl.createUser = async (req, res) => {
+
+    const { username } = req.body;
+
+    const newUser = new UserModel({
+        username
+    });
+
+    await newUser.save();
+
+    res.json({
+        message: 'Usuario guardado'
+    }
+)};
+
+userCtrl.deleteUser = async (req, res) => {
+
+    const id = req.params.id;
+
+    await UserModel.findByIdAndDelete(id);
+
+    res.json({
+        message: 'Usuario borrado!'
+    }
+)};
 
 module.exports = userCtrl;
